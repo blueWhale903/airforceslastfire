0d84b1348272a1a120ba8dc7fa0e4d75abf1b8f2
hgl <longhg903@gmail.com>
2026-05-08 16:01:47 +0700

    fix enemy4 laser shoot animation

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -4,18 +4,15 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-
     [SerializeField] Animator SceneTransition;
+
     public void Play()
     {
-        Debug.Log("in1");
-        //SceneManager.LoadScene("Level 1");
         StartCoroutine(TransitionAndLoad("Level 1"));       
     }
 
     private IEnumerator TransitionAndLoad(string sceneName)
     {
-        Debug.Log("in2");
         SceneTransition.Play("SceneTransitionReverse");
 
         yield return new WaitForSeconds(1);
